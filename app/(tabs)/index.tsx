571660a64d537934ad30b01c3c3afd7a76a7bd91
Jowell <jowell@Jowells-MacBook-Pro-2.local>
2024-07-16 16:51:17 -0400

    First

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,52 +1,46 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
+import { Image, StyleSheet, Platform, Button, View, Text, NativeEventEmitter, NativeModules } from 'react-native';
+import {useState} from 'react';
+import BleManager, {Peripheral} from 'react-native-ble-manager';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
+  
+  const [deviceNames,setDeviceNames] = useState<string[]>([]);
+  const bleManagerEmitter = new NativeEventEmitter(NativeModules.BleManager);
+  bleManagerEmitter.addListener('BleManagerDiscoverPeripheral', (peripheral)=>{handleDiscoverDevice(peripheral);})
+  BleManager.start();
+
+  function handleDiscoverDevice(peripheral: Peripheral)
+  {
+    
+    if(peripheral.name)
+      {
+        setDeviceNames([...deviceNames,peripheral.name]);
+      }  
+      
+    
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View>
+     <Text>----</Text>
+    <Text>----</Text>
+    <Text>-----</Text>
+    <Text>----</Text>
+    <Text>----</Text>
+    <Text>-----</Text>
+    
+    
+   
+    <Button onPress={()=>{console.log("Pressed");BleManager.scan([],180,false);}} title="Scan"/>
+    {deviceNames.map((item)=>{
+      return(<Button title={item} onPress={()=>{alert("Do you want to connect to "+item+"?")}}/>);
+    })}
+    
+    </View>
   );
 }
 
